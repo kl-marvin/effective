e10a2b4f16388af8c599fa8d3ef446ea82650e24
kl-marvin <kl.marvin@outlook.com>
2019-10-31 11:10:55 +0100

    registration action added

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -5,10 +5,45 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use App\Entity\User;
+use App\Form\UserRegistrationType;
+use App\Repository\UserRepository;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
 
 class SecurityController extends AbstractController
 {
+
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $encoder;
+
+    /**
+     * @var ObjectManager
+     */
+    private $em;
+
+    /**
+     * @var \Twig_Environment
+     */
+    private $twig_Environment;
+  
+    /**
+     * @param ObjectManager $em
+     */
+    public function __construct(ObjectManager $em, UserPasswordEncoderInterface $encoder, \Swift_Mailer $mailer, \Twig_Environment $twig_Environment)
+    {
+        $this->em = $em;
+        $this->mailer = $mailer;
+        $this->twig_Environment = $twig_Environment;
+        $this->encoder = $encoder;
+    }
+
+
     /**
      * @Route("/login", name="app_login")
      */
@@ -32,5 +67,31 @@ class SecurityController extends AbstractController
     public function logout()
     {
         throw new \Exception('This method can be blank - it will be intercepted by the logout key on your firewall');
+    }
+
+
+    /**
+     * @Route("/registration", name="app_registration")
+     */
+    public function registration(Request $request)
+    {
+            $user = new User();
+            $form = $this->createForm(UserRegistrationType::class, $user);
+            $form->handleRequest($request);
+
+
+            if($form->isSubmitted() && $form->valid()){
+                $user->setRole(['ROLE_USER']);
+                $user->setPassword($this->encoder->encodePassword($user, $user->getPassword()));
+                $this->em->persist($user);
+                $this->em->flush();
+            }
+
+            return $this->render('security/registration.html.twig', [
+                'form' => $form->createView(),
+                'user' => $this->getUser()
+            ]);
+          
+
     }
 }
--- src/Form/UserRegistrationType.php
+++ src/Form/UserRegistrationType.php
@@ -6,16 +6,26 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class UserRegistrationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
-            ->add('name')
+        ->add('email', EmailType::class, [
+            'label' => 'Votre Email',
+            'required' => true
+        ])
+        ->add('password', PasswordType::class, [
+            'label' => 'Mot de Passe',
+             'required' => true
+        ])
+        ->add('name', null, [
+            'label' => 'Nom',
+            'required' => true
+        ])
         ;
     }
 
